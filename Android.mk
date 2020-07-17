2b8f5860e2914a42ad0ea6d44364f5640aca695c
yongchun.li <yongchun.li@amlogic.com>
2020-07-16 18:02:05 -0700

    aucpu_fw: HW validation firmware with DEBUG enabled [1/1]
    PD#SWPL-16674
    
    Problem:
    Need prebuild aucpu FW for SC2 related projects.
    
    Solution:
    Seperate git holder for aucpu firmware
    
    Verify:
    ohm AH212
    
    Change-Id: I9a9eea78210f7969d86b26b611fe07205ff9691a
    Signed-off-by: yongchun.li <yongchun.li@amlogic.com>

---
+++ Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+
+FW_BINARY=aucpu_fw
+FW_EXPORT_DIR := $(PRODUCT_OUT)/vendor/lib/firmware
+
+include $(CLEAR_VARS)
+
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := aucpu_fw
+LOCAL_SRC_FILES := aucpu_fw.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX := .bin
+LOCAL_MODULE_PATH := $(FW_EXPORT_DIR)
+LOCAL_STRIP_MODULE := false
+include $(BUILD_PREBUILT)
