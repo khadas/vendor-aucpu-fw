c65ed159625580adc49f2b9c638b018ae641e2a2
Lawrence Mok <lawrence.mok@amlogic.com>
2021-05-05 12:18:59 -0700

    aucpu: support secure aucpu fw loading [4/4]
    PD#SWPL-49064
    
    Problem:
    Need to support secure aucpu fw loading
    
    Solution:
    Add signed/encrypted aucpu fw
    
    Add Signed AUCPU FW release 1.0.1 for SC2
    
    Verify:
    insmod aucpu_fw.ko, check fw is loaded
    
    Signed-off-by: Lawrence Mok <lawrence.mok@amlogic.com>
    Change-Id: Ib2f3e8fd5061da3c33cd9497e1d00d5061686733

--- Android.mk
+++ Android.mk
@@ -3,11 +3,17 @@ LOCAL_PATH:= $(call my-dir)
 FW_BINARY=aucpu_fw
 FW_EXPORT_DIR := $(PRODUCT_OUT)/vendor/lib/firmware
 
+ifeq ($(filter S905C2 S905C2ENG S905X4, $(BOARD_AML_SOC_TYPE)),)
+	LOCAL_FW := aucpu_fw.bin
+else
+	LOCAL_FW := $(BOARD_AML_SOC_TYPE)/aucpu_fw.bin.signed
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE := aucpu_fw
-LOCAL_SRC_FILES := aucpu_fw.bin
+LOCAL_SRC_FILES := $(LOCAL_FW)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .bin
